c40d84e0c979f21a66c893935db657a60f0cb5c8
Kevin Wagenseil <kwagenseil@gmail.com>
2015-04-01 15:08:17 -0400

    Getting started with Enemies

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(x,y,speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function(dt) {         // this add a method called "update" to our previously defined Enemy class
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x < canvas.width) {
+      this.x += this.speed * dt;
+    } else {
+      this.x = 0;
+      this.y = ;
+      this.speed =
+    }
+    }
 }
 
 // Draw the enemy on the screen, required method for game
@@ -21,15 +27,20 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x,y) {
 
+  this.sprite = 'images/char-horn-girl.png'
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
+var player = ;
 
 
 // This listens for key presses and sends the keys to your
