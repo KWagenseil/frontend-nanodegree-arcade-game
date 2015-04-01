f755eba2e6edbdf0d4f1a0c0bcaba43eafaa1be7
Kevin Wagenseil <kwagenseil@gmail.com>
2015-04-01 17:04:27 -0400

    Adding player handleInput

--- js/app.js
+++ js/app.js
@@ -1,46 +1,83 @@
-// Enemies our player must avoid
-var Enemy = function(x,y,speed) {
+/* var startRow = function() {       // finish this later
+
+}
+
+var startSpeed = function() {       // finish this later
+
+}
+*/
+
+// Enemies our player must avoid (COMPLETE)
+function Enemy(x,y,speed) {
     this.x = x;
     this.y = y;
     this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {         // this add a method called "update" to our previously defined Enemy class
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    if (this.x < canvas.width) {
+// Update enemy location based on movement, reaching the end, or colliding with the hero
+Enemy.prototype.update = function(dt) {                           // This adds a new method called "update" to our previously defined Enemy class.
+    if (this.x < /*ctx.*/canvas.width) {                          // "dt" is found in the engine.js file
       this.x += this.speed * dt;
+    } else if (this.x && this.startRow(); === Player.x && Player.y) {         // Trying to add a collision update with 'player.position'
+      this.x = 0;
+      this.y = randomRow();
+      this.speed = startSpeed();
     } else {
       this.x = 0;
-      this.y = ;
-      this.speed =
+      this.y = randomRow();
+      this.speed = startSpeed();
     }
-    }
-}
+};
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen, required method for game (COMPLETE)
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+// handleInput() method.
+
+function Player(x,y) {                                // New Player class function
+    this.x = x;
+    this.y = y;
+    /* this.sprite = add option to chose hero; */
+    this.sprite = 'images/char-horn-girl.png'
+};
+
+Player.prototype.update = function(dt) {              // Player update location
+    if (this.y > /*ctx.*/canvas.height) || (this.x && this.y; === Enemy.x && Enemy.startRow();) {
+      player.startOver();
+    }
+};
+
+Player.prototype.render = function() {                // Loads the hero to the screen
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(arrow) {
+  if (arrow === "left") {
+      this.x -= /*pixels left*/;
+  } if
+     (arrow === "up") {
+       this.y -= /*pixels up*/;
+  } if
+     (arrow === "right") {
+       this.x += /*pixels right*/;
+  } if
+     (arrow === "down") {
+       this.y += /*pixels down*/;
+     };
 }
 
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function(x,y) {
 
-  this.sprite = 'images/char-horn-girl.png'
-}
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-var player = ;
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
